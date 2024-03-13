9ab10009850236e82ba922ae8b2ee63dcbff3685
Pedro Valls <pedro.valls@fgv.br>
2024-03-13 08:31:41 -0300

    Add files via upload

---
+++ MCE.R
@@ -0,0 +1,241 @@
+##
+# Dynamic Models Typology  - MCE
+##
+
+# clear workspace
+rm(list=ls())
+# set local directory to where data set is 
+# setwd("C:/Users/Pedro/Dropbox/EcoIII2020/Lecture4_Unit_Root")
+
+# Load package using a function load_package-----------------------------------------------------------------
+load_package<-function(x){
+  x<-as.character(match.call()[[2]])
+  if (!require(x,character.only=TRUE)){
+    install.packages(pkgs=x,repos="http://cran.r-project.org")
+    require(x,character.only=TRUE)
+  }
+}
+
+load_package("tseries")
+load_package("astsa")
+load_package("forecast")
+load_package("urca")
+load_package("xtable")
+load_package("readxl")
+load_package("stats")
+load_package("lmtest")
+load_package("ggplot2")
+load_package("sandwich")
+load_package("zoo")
+load_package("car")
+
+library(xtable)
+library(forecast)
+library(tseries) 
+library(urca)
+library(astsa)
+library(stats)
+library(lmtest) # for linear regression diagnostics
+library(ggplot2) # for plotting
+library(sandwich)
+library(zoo)
+library(car)
+
+setwd("C:/Users/Pedro/Dropbox/EcoIII2021/Lecture6_adl/R_code")
+
+datagive1 <- read_excel("datagive1.xlsx")
+class(datagive1)
+
+
+# Plotting the beta coefficients
+par(mfrow=c(1,3))
+plot(datagive1$Date[2:159], datagive1$cons[2:159], type = "l", col= "red", main = "Cons", ylab="cons", xlab="Date")
+plot(datagive1$Date[2:159], datagive1$inc[2:159], type = "l", col= "red", main = "Inc", ylab="inc", xlab="Date")
+
+plot(datagive1$Date[2:159], datagive1$inflat[2:159], type = "l", col= "red", main = "Inflat", ylab="inflat", xlab="Date")
+##
+# Correlation between CONS INC INFLAT
+##
+new_data = cbind.data.frame(datagive1$cons,datagive1$cons1,datagive1$inc,datagive1$inc1,datagive1$inflat,datagive1$inflat1)
+new_data = new_data[2:159,]
+cor_new_data = cor(new_data,method=c("spearman"))
+cor_new_data
+
+
+##
+# ADL(1,1)
+##
+# Perform linear regression using data from2 to 159 
+model_ADL<- lm(log(cons) ~ log(cons1)+log(inc)+log(inc1), data = datagive1[2:159,])
+
+# Summary of the model to display regression results
+summary(model_ADL)
+
+
+
+
+# Using White correction
+coeftest(model_ADL, vcov = vcovHC(model_ADL, type = "HC0"))
+#using Newey-West Correction
+coeftest(model_ADL, vcov = vcovHAC(model_ADL))
+
+num = length(datagive1$cons)-1
+num
+AIC_ADL= AIC(model_ADL)
+AIC_ADL
+AIC_ADL_T=AIC_ADL/num
+AIC_ADL_T
+BIC_ADL = BIC(model_ADL)
+
+BIC_ADL
+BIC_ADL_T=BIC_ADL/num
+BIC_ADL_T
+npar_ADL = length(model_ADL$coefficients)
+loglik_ADL = -(1/2)*(AIC_ADL - 2*npar_ADL)
+loglik_ADL
+
+# Create a data frame of predicted values and standard errors
+predictions_ADL <- data.frame(
+  lcons_ADL = log(datagive1$cons[2:159]),
+  Predicted_ADL = predict(model_ADL, interval = "prediction")[,1],
+  Lower_ADL = predict(model_ADL, interval = "prediction")[,2],
+  Upper_ADL = predict(model_ADL, interval = "prediction")[,3]
+)
+
+# Save  residuals 
+Residuals_ADL <- residuals(model_ADL)
+
+# Plot observed vs predicted values with confidence intervals
+
+
+
+ggplot(predictions_ADL, aes(x = datagive1$Date[2:159])) +
+  geom_line(aes(y = Predicted_ADL, color = "Predicted"), size = 1) +  # Define color within aes for legend
+  geom_ribbon(aes(ymin = Lower_ADL, ymax = Upper_ADL, fill = "Confidence Interval"), alpha = 0.2) +  # Define fill within aes for legend
+  geom_point(aes(y = lcons_ADL, color = "Observed"), size = 2) +  # Define color within aes for legend
+  scale_color_manual(values = c("Predicted" = "blue", "Observed" = "red")) +  # Manual color values
+  scale_fill_manual(values = c("Confidence Interval" = "grey")) +  # Manual fill values
+  labs(title = "Observed vs Predicted", x = "Date", y = "Value", 
+       color = "Legend", fill = "Legend") +  # Customize legend title
+  theme_minimal()
+
+
+# Plot residuals with 2*sigma^{2}
+sigma_value_ADL = sigma(model_ADL)
+sigma_value_ADL
+
+ggplot(predictions_ADL, aes(x = datagive1$Date[2:159], y = Residuals_ADL)) +
+  geom_point(aes(color = "Residuals")) +  # Assign color within aes for legend
+  geom_line(aes(color = "Residuals")) +  # Use same color as points to keep them in a single legend item
+  geom_hline(aes(yintercept = 2 * sigma_value_ADL, linetype = "2*sigma"), color = "blue") +  # Map linetype to aes
+  geom_hline(aes(yintercept = -2 * sigma_value_ADL, linetype = "-2*sigma"), color = "gray") +  # Map linetype to aes
+  scale_color_manual(values = c("Residuals" = "red")) +  # Manual color values for points and lines
+  scale_linetype_manual(values = c("2*sigma" = "dashed", "-2*sigma" = "dashed")) +  # Manual linetype values
+  labs(title = "Residuals", x = "Date", y = "residuals", color = "Legend", linetype = "Legend") +  # Custom legend titles
+  theme_minimal()
+
+
+
+###
+# Test log run elasticity and Homogeneity
+##
+
+
+test_log_run_elasticity_chi <-linearHypothesis(model_ADL,c("log(cons1) = 1.0","log(inc) = -log(inc1)"), test ="Chisq")
+test_log_run_elasticity_F <-linearHypothesis(model_ADL,c("log(cons1) = 1.0","log(inc) = -log(inc1)"), test ="F")
+test_log_run_elasticity_chi
+test_log_run_elasticity_F
+
+
+##
+# Test Omitted Variables: Inflat and Inflat1
+##
+
+##
+# Unrrestricted model with Inflta and Inflat1
+##
+
+model_ADL_Inflat<- lm(log(cons) ~ log(cons1)+log(inc)+log(inc1)+inflat+inflat1, data = datagive1[2:159,])
+
+# Summary of the model to display regression results
+summary(model_ADL_Inflat)
+
+
+
+# Using White correction
+coeftest(model_ADL_Inflat, vcov = vcovHC(model_ADL_Inflat, type = "HC0"))
+#using Newey-West Correction
+coeftest(model_ADL_Inflat, vcov = vcovHAC(model_ADL_Inflat))
+
+num = length(datagive1$cons)-1
+num
+AIC_ADL_Inflat= AIC(model_ADL_Inflat)
+AIC_ADL_Inflat
+AIC_ADL_Inflat_T=AIC_ADL_Inflat/num
+AIC_ADL_Inflat_T
+BIC_ADL_Inflat = BIC(model_ADL_Inflat)
+
+BIC_ADL_Inflat
+BIC_ADL_Inflat_T=BIC_ADL_Inflat/num
+BIC_ADL_Inflat_T
+npar_ADL_Inflat = length(model_ADL_Inflat$coefficients)
+loglik_ADL_Inflat = -(1/2)*(AIC_ADL_Inflat - 2*npar_ADL_Inflat)
+loglik_ADL_Inflat
+
+
+
+# Create a data frame of predicted values and standard errors
+predictions_ADL_Inflat <- data.frame(
+  lcons_ADL_Inflat = log(datagive1$cons[2:159]),
+  Predicted_ADL_Inflat = predict(model_ADL_Inflat, interval = "prediction")[,1],
+  Lower_ADL_Inflat = predict(model_ADL_Inflat, interval = "prediction")[,2],
+  Upper_ADL_Inflat = predict(model_ADL_Inflat, interval = "prediction")[,3]
+)
+
+# Save  residuals 
+Residuals_ADL_Inflat <- residuals(model_ADL_Inflat)
+
+# Plot observed vs predicted values with confidence intervals
+
+
+
+ggplot(predictions_ADL_Inflat, aes(x = datagive1$Date[2:159])) +
+  geom_line(aes(y = Predicted_ADL_Inflat, color = "Predicted"), size = 1) +  # Define color within aes for legend
+  geom_ribbon(aes(ymin = Lower_ADL_Inflat, ymax = Upper_ADL_Inflat, fill = "Confidence Interval"), alpha = 0.2) +  # Define fill within aes for legend
+  geom_point(aes(y = lcons_ADL_Inflat, color = "Observed"), size = 2) +  # Define color within aes for legend
+  scale_color_manual(values = c("Predicted" = "blue", "Observed" = "red")) +  # Manual color values
+  scale_fill_manual(values = c("Confidence Interval" = "grey")) +  # Manual fill values
+  labs(title = "Observed vs Predicted - Model with Inflat and Inflat1", x = "Date", y = "Value", 
+       color = "Legend", fill = "Legend") +  # Customize legend title
+  theme_minimal()
+
+
+# Plot residuals with 2*sigma^{2}
+sigma_value_ADL_Inflat = sigma(model_ADL_Inflat)
+sigma_value_ADL_Inflat
+
+ggplot(predictions_ADL_Inflat, aes(x = datagive1$Date[2:159], y = Residuals_ADL_Inflat)) +
+  geom_point(aes(color = "Residuals")) +  # Assign color within aes for legend
+  geom_line(aes(color = "Residuals")) +  # Use same color as points to keep them in a single legend item
+  geom_hline(aes(yintercept = 2 * sigma_value_ADL_Inflat, linetype = "2*sigma"), color = "blue") +  # Map linetype to aes
+  geom_hline(aes(yintercept = -2 * sigma_value_ADL_Inflat, linetype = "-2*sigma"), color = "gray") +  # Map linetype to aes
+  scale_color_manual(values = c("Residuals" = "red")) +  # Manual color values for points and lines
+  scale_linetype_manual(values = c("2*sigma" = "dashed", "-2*sigma" = "dashed")) +  # Manual linetype values
+  labs(title = "Residuals Model with Inflat and Inflat1", x = "Date", y = "residuals", color = "Legend", linetype = "Legend") +  # Custom legend titles
+  theme_minimal()
+
+
+
+
+
+
+
+###
+# Test Ommited Variables
+##
+
+
+test_log_run_elasticity_chi <-linearHypothesis(model_ADL_Inflat,c("inflat = 0.0","inflat1 = 0"), test ="Chisq")
+test_log_run_elasticity_F <-linearHypothesis(model_ADL_Inflat,c("inflat = 0.0","inflat1 = 0"), test ="F")
+test_log_run_elasticity_chi
+test_log_run_elasticity_F
